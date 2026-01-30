1e8f0e96e69aaac4055ef346d13e0616a3e05946
Mauriciocbx94 <mauriciocbx1994@gmail.com>
2026-01-29 23:22:47 -0300

    Delete notasalunos.py

--- notasalunos.py
+++
@@ -1,7 +0,0 @@
-print('Olá, vamos calcular a nota do aluno!')
-nota1 = int(input('Digite a primeira nota: '))
-nota2 = int(input('Digite a segunda nota: '))
-
-nota_final = nota1 + nota2 
-
-print(f'A média final do aluno foi de {nota_final}, parabéns !')
