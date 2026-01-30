7bcf07ddf853c02c97b6d03a2622b43c4bfc2c0e
Mauriciocbx94 <mauriciocbx1994@gmail.com>
2026-01-29 23:07:36 -0300

    Calculadora

---
+++ Calculadora.py
@@ -0,0 +1,9 @@
+n1 = int(input('um valor: '))
+n2 = int(input('Outro valor: '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+print(f'A soma é {s}, \n a multiplicação é {m}, \n a divisão é {d:.2f}')
+print(f'Divisão inteira {di} e potencia {e:.3f}')
